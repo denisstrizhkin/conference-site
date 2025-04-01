162b6d6bcc65cea52ff59899c629f89f5b43d7c6
Denis Strizhkin <strdenis02@gmail.com>
2025-04-01 21:47:13 +0300

    user pass field

--- src/routers/user/models.py
+++ src/routers/user/models.py
@@ -4,3 +4,4 @@ from sqlmodel import SQLModel, Field
 class User(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     name: str = Field(default=None, nullable=False)
+    password: str = Field(default=None, nullable=False)
