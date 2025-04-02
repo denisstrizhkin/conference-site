0726c710fa4852f988cdafc0ba563412a8b5ac38
Denis Strizhkin <strdenis02@gmail.com>
2025-04-02 18:23:43 +0300

    add user

--- src/routers/user/models.py
+++ src/routers/user/models.py
@@ -3,5 +3,5 @@ from sqlmodel import SQLModel, Field
 
 class User(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
-    name: str = Field(default=None, nullable=False)
+    email: str = Field(default=None, nullable=False)
     password: str = Field(default=None, nullable=False)
--- src/routers/user/router.py
+++ src/routers/user/router.py
@@ -1,20 +1,43 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, Path, Query, status, Request, Form
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
+from sqlmodel import select
 
+from src.db import AsyncSession
 from src.depends import Templates
+from .models import User
 
 router = APIRouter()
 
 
-@router.get("/register")
-def register(templates: Templates, request: Request):
+@router.get("/register", response_class=HTMLResponse)
+async def register(templates: Templates, request: Request):
     return templates.TemplateResponse(
         request=request, name="user/register.jinja", context={"title": "StudConfAU"}
     )
 
 
 @router.post("/register")
-def create_user(email: Annotated[str, Form()], password: Annotated[str, Form()]):
-    return {"email": email}
+async def create_user(
+    email: Annotated[str, Form()],
+    password: Annotated[str, Form()],
+    session: AsyncSession,
+):
+    user = User(email=email, password=password)
+    async with session() as session:
+        session.add(user)
+        await session.commit()
+    return RedirectResponse(url="/user/register", status_code=status.HTTP_303_SEE_OTHER)
+
+
+@router.get("/", response_class=HTMLResponse)
+async def get_users(templates: Templates, request: Request, session: AsyncSession):
+    async with session() as session:
+        result = await session.execute(select(User))
+    users = result.scalars().all()
+    return templates.TemplateResponse(
+        request=request,
+        name="user/list.jinja",
+        context={"title": "StudConfAU", "users": users},
+    )
